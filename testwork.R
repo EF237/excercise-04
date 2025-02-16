c7809f296bc54651b9cb0100532fbd74c4f820b8
EF237 <jjamesrollins237@utexas.edu>
2025-02-16 17:40:52 -0600

    initial files

---
+++ testwork.R
@@ -0,0 +1,20 @@
+
+# Learning objectives
+
+# Loading data files into R
+# Breaking a programming challenge down into discrete steps
+# Writing novel functions
+# Using arguments in functions
+# Using set operation functions
+# Using loops and conditional statements
+# Working with different data structures (vectors, tabular data)
+# Practicing data wrangling
+# Dealing with keyboard input
+
+##################################
+
+# Data Files
+
+# collins-scrabble-words-2019.txt    # "words"
+# google-10000-english-usa-no-swears.txt
+
