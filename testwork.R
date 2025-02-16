f0721d0d0af51bba2a64a48d9c5f7f32c4223a10
EF237 <jjamesrollins237@utexas.edu>
2025-02-16 17:52:37 -0600

    added data files

--- testwork.R
+++ testwork.R
@@ -13,8 +13,15 @@
 
 ##################################
 
+library(tidyverse)
+library(dplyr)
+
 # Data Files
 
 # collins-scrabble-words-2019.txt    # "words"
-# google-10000-english-usa-no-swears.txt
+A <- read_csv("https://raw.githubusercontent.com/difiore/ada-datasets/refs/heads/main/collins-scrabble-words-2019.txt")
+
+# google-10000-english-usa-no-swears.txt  # "words"
+B <- read_csv("https://raw.githubusercontent.com/difiore/ada-datasets/refs/heads/main/google-10000-english-usa-no-swears.txt")
+
 
